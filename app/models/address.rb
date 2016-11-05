5885b3d68deed265bb56cf59fb8f913b6de8d5d8
Prelang Builder <builder@prelang.com>
2016-11-05 00:03:20 +0000

    Defined validation for attribute 'phone_number' on model 'Address'

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,5 @@ class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :fulfillment_options
   validates_formatting_of :zip, using: :us_zip
+  validates_formatting_of :phone_number, using: :us_phone
 end
